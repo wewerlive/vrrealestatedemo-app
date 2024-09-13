6a5add76c5f305c4fd8274edd49eac40a8ff1fcf
Bhaumik Singh <Bhaumiksingh2000@gmail.com>
2024-09-13 22:16:33 +0530

    Update WebSocket URL in DevicesPage to use secure connection
    Signed-off-by: Bhaumik Singh <Bhaumiksingh2000@gmail.com>

--- lib/screens/device_page.dart
+++ lib/screens/device_page.dart
@@ -107,7 +107,7 @@ class _DevicesPageState extends State<DevicesPage> {
   }
 
   void _initWebSocket(String deviceId) {
-    final wsUrl = Uri.parse('ws://10.140.0.228:8080/server/socket/$deviceId');
+    final wsUrl = Uri.parse('wss://vrerealestatedemo-backend.globeapp.dev/server/socket/$deviceId');
     socketConnections[deviceId] = WebSocketChannel.connect(wsUrl);
 
     socketConnections[deviceId]!.stream.listen((message) {
